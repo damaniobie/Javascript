fb7eff94bfcd57dcf0762cbc758e096bfe9a7b25
damaniobie <35574248+damaniobie@users.noreply.github.com>
2018-03-24 14:39:15 -0400

    Create RepeatedNumbers.js

---
+++ PracticeProblems/RepeatedNumbers.js
@@ -0,0 +1,32 @@
+//Locate all repeating numbers in a given number of digits. The size of the number that gets repeated should be more than 1
+
+
+var aString = "82156821568221";
+var aList = []
+
+// Build a list
+for (let i = 0; i < aString.length; i++) {
+    var j = i;
+    while (j < aString.length) {
+        j++;
+        var sub = aString.slice(i, j);
+        if (sub.length >= 2) {
+            aList.push(sub)
+        }
+    }
+}
+// Associative array
+var mapped = aList.reduce(function(prev, cur) {
+    prev[cur] = (prev[cur] || 0) + 1;
+    return prev;
+},{});
+
+// Print it.
+for (var key in mapped) {
+    if (mapped[key] >= 2) {
+        console.log(key + ": " + mapped[key]);
+    }
+};
+
+///////////TEST////////////////
+/* TO TEST, SIMPLY RUN THE PROGRAM */
